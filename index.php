d490b80bf657bf64a2a738d53c7d32fe1b12aa4b
alexparavi <lexaparavin@gmail.com>
2026-04-15 14:55:10 +0400

    chore: practice foreach and alcohol calculation logic

--- index.php
+++ index.php
@@ -19,4 +19,20 @@ foreach ($guestData as $guest) {
     if ($guest['is_confirmed'] === true) {
         echo "{$guest['name']} идёт на свадьбу!  Напитков выбрано:" .  count($guest['alcohol']) . PHP_EOL;
     }
-}
+};
+
+foreach ($guestData as $guest) {
+    if ($guest['side'] === 'groom') {
+        echo "{$guest['name']} со стороны жениха" . PHP_EOL;
+    } else {
+        echo "{$guest['name']} со стороны невесты" . PHP_EOL;
+    }
+};
+$totalDrinks = 0;
+foreach ($guestData as $guest) {
+    $totalDrinks += count($guest['alcohol']);
+    $totalAlcoholType = $guest['alcohol'];
+    $alcoholList = implode(",", $totalAlcoholType);
+};
+
+print_r("Всего надо купить {$totalDrinks} бутылок") . PHP_EOL;
